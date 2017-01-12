09edea30cfb2cf805d493e12655ceaadeb1e1949
Christian Martinez <ian.martinez@na.drillinginfo.com>
2017-01-11 22:09:29 -0600

    Added multi user query support.

--- pyadquery.py
+++ pyadquery.py
@@ -3,12 +3,22 @@ import click
 
 
 @click.command()
-@click.option('--fn', prompt='Enter the First Name', help="i.e. John")
-@click.option('--ln', prompt='Enter the Last Name', help='i.e Doe')
-def queryexpiration(fn,ln):
-    cn = "{} {}".format(fn,ln)
-    user = aduser.ADUser.from_cn(cn)
-    print user.get_password_last_set()
+@click.option('--names', default='', help='Enter a list of Names')
+@click.option('--fn', default='', help="i.e. John")
+@click.option('--ln', default='', help='i.e Doe')
+def query_expiration(names,fn,ln):
+    if fn:
+        cn = "{} {}".format(fn,ln)
+        user = aduser.ADUser.from_cn(cn)
+        print "{}'s expiration date is {}".format(cn, user.get_password_last_set())
+    elif names:
+        name_list = names.split(",")
+        for name in name_list:
+            user = aduser.ADUser.from_cn(name)
+            print "{}'s expiration date is {}".format(name,user.get_password_last_set())
+    else:
+        print "No names given."
+
 
 if __name__ == '__main__':
-    queryexpiration()
+    query_expiration()
